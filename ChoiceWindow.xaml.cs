647bb2ab18eb0a995e6b690557770775464b3d38
ramanan padmanabhlan <ramanan.padmanabhlan@kohls.com>
2012-05-23 13:25:25 -0500

    Initial commit

---
+++ ChoiceWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Translator.WPF;
+namespace Communication.WPF {
+    /// <summary>
+    /// Interaction logic for ChoiceWindow.xaml
+    /// </summary>
+    public partial class ChoiceWindow : ACommunicationWindow {
+        public ChoiceWindow(string title, string message, List<string> options, string default_option, ICommunicationReceiver owner)
+            : base(owner) {
+            InitializeComponent();
+            TranslationHelpers.translateWindow(this);
+            int selected = 0;
+            this.Title = title;
+            messageGrp.Header = message;
+            foreach (string add_me in options) {
+                choiceCombo.Items.Add(add_me);
+                if (add_me == default_option)
+                    choiceCombo.SelectedIndex = selected;
+                selected++;
+            }
+        }
+
+
+        public string selected_item {
+            get {
+                return choiceCombo.Text;
+            }
+        }
+
+        public int selected_index {
+            get {
+                return choiceCombo.SelectedIndex;
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ MessageBox.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using Email;
+using Translator;
+using Translator.WPF;
+namespace Communication.WPF {
+    /// <summary>
+    /// Interaction logic for MessageBox.xaml
+    /// </summary>
+    public partial class MessageBox : ACommunicationWindow {
+
+        public MessageBox(string title, string message, ICommunicationReceiver owner)
+            : base(owner) {
+            InitializeComponent();
+            TranslationHelpers.translateWindow(this);
+            this.Title = title;
+            messageLabel.Content = message;
+            if (owner != null)
+                this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+            else
+                this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        public MessageBox(string title, string message, RequestType type, ICommunicationReceiver owner)
+            : this(title, message, owner) {
+            if (type == RequestType.Question) {
+                cancelButton.Visibility = System.Windows.Visibility.Visible;
+                submitButton.Visibility = System.Windows.Visibility.Collapsed;
+                TranslationHelpers.translate(okButton,"Yes");
+                TranslationHelpers.translate(cancelButton,"No");
+                questionIcon.Visibility = System.Windows.Visibility.Visible;
+                exceptionExpander.Visibility = System.Windows.Visibility.Collapsed;
+            } else {
+                this.DialogResult = false;
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public MessageBox(string title, string message, Exception e, MessageTypes type, ICommunicationReceiver owner)
+            : this(title, message, owner) {
+            switch (type) {
+                case MessageTypes.Error:
+                    cancelButton.Visibility = System.Windows.Visibility.Collapsed;
+                    if (e != null) {
+                        exceptionExpander.Visibility = System.Windows.Visibility.Visible;
+                        exceptionText.Text = recurseExceptions(e);
+                        if (e.GetType() == typeof(CommunicatableException)) {
+                            submitButton.Visibility = System.Windows.Visibility.Visible;
+                            //                            if(!((CommunicatableException)e).submittable)
+                            //                              submitButton.Visibility = System.Windows.Visibility.Collapsed;
+                        } else {
+                            submitButton.Visibility = System.Windows.Visibility.Visible;
+                        }
+                    } else {
+                        submitButton.Visibility = System.Windows.Visibility.Collapsed;
+                        exceptionExpander.Visibility = System.Windows.Visibility.Collapsed;
+                    }
+                    TranslationHelpers.translate(okButton,"Close");
+                    errorIcon.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case MessageTypes.Info:
+                    cancelButton.Visibility = System.Windows.Visibility.Collapsed;
+                    exceptionExpander.Visibility = System.Windows.Visibility.Collapsed;
+                    submitButton.Visibility = System.Windows.Visibility.Collapsed;
+                    TranslationHelpers.translate(okButton,"OK");
+                    infoIcon.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case MessageTypes.Warning:
+                    cancelButton.Visibility = System.Windows.Visibility.Collapsed;
+                    exceptionExpander.Visibility = System.Windows.Visibility.Collapsed;
+                    submitButton.Visibility = System.Windows.Visibility.Collapsed;
+                    TranslationHelpers.translate(okButton,"OK");
+                    warningIcon.Visibility = System.Windows.Visibility.Visible;
+                    break;
+            }
+        }
+
+        public static string recurseExceptions(Exception e) {
+            StringBuilder return_me = new StringBuilder(e.Message);
+            return_me.AppendLine();
+            return_me.AppendLine();
+
+            return_me.AppendLine(e.StackTrace);
+            if (e.InnerException != null) {
+                return_me.AppendLine(recurseExceptions(e.InnerException));
+                return_me.AppendLine();
+            }
+            return return_me.ToString();
+            ;
+        }
+
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+        }
+        private EmailHandler email;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            if (submitButton.Visibility == System.Windows.Visibility.Visible) {
+                //                email = new EmailHandler();
+                //              email.checkAvailability(checkAvailabilityDone);
+                TranslationHelpers.translate(submitButton,"CheckingConnection");
+            }
+        }
+
+        private void checkAvailabilityDone(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e) {
+            if (email.email_available) {
+                submitButton.IsEnabled = true;
+                TranslationHelpers.translate(submitButton,"SendReport");
+            } else {
+                submitButton.IsEnabled = false;
+                TranslationHelpers.translate(submitButton,"CantSendReport");
+            }
+
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e) {
+            if (!this.checkEmail())
+                return;
+
+            StringBuilder body = new StringBuilder();
+            body.AppendLine(this.Title);
+            body.AppendLine();
+            body.AppendLine(messageLabel.Content.ToString());
+            body.AppendLine();
+            body.AppendLine(exceptionText.Text);
+            body.AppendLine();
+
+            submitButton.IsEnabled = false;
+            TranslationHelpers.translate(submitButton,"SendingReport");
+            email.sendEmail("submissions@masgau.org", "submissions@masgau.org", email_config.email, "MASGAU Error - " + this.Title, body.ToString(), sendEmailDone);
+        }
+
+        private void sendEmailDone(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e) {
+            if (e.Error != null) {
+                TranslationHelpers.translate(submitButton,"SendFailed");
+                displayError("Error time", e.Error.Message);
+            } else {
+                TranslationHelpers.translate(submitButton,"ReportSent");
+            }
+            submitButton.IsEnabled = false;
+        }
+    }
+}
