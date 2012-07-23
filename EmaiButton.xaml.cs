4cb79b6cf6116603de660d5649fd33c6b4bfc7f4
Matthew Barbour <sanmadjack@gmail.com>
2012-07-23 16:53:53 -0500

    Added beginnings of email button

---
+++ EmaiButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Communication.WPF {
+    /// <summary>
+    /// Interaction logic for EmaiButton.xaml
+    /// </summary>
+    public partial class EmaiButton : UserControl {
+        public EmaiButton() {
+            InitializeComponent();
+        }
+    }
+}
