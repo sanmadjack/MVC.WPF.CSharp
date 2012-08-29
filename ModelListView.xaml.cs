df45f27774860ffc175cfa1a9e0a9408e1c61099
Matthew Barbour <sanmadjack@gmail.com>
2012-08-29 16:41:56 -0500

    Added new Model List View stuff

---
+++ ModelListView.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Windows.Controls;
+namespace MVC.WPF {
+    /// <summary>
+    /// Interaction logic for ModelListView.xaml
+    /// </summary>
+    public partial class ModelListView : UserControl {
+        public ModelListView() {
+            InitializeComponent();
+        }
+
+        public event SelectionChangedEventHandler SelectionChanged;
+
+        public ModelListViewItem TemplateItem = new ModelListViewItem();
+
+        protected INotifyingCollection _model;
+        public INotifyingCollection Model {
+            get {
+                return _model;
+            }
+            set {
+                if (_model != null) {
+                    _model.CollectionChanged -= new System.Collections.Specialized.NotifyCollectionChangedEventHandler(_model_CollectionChanged);
+                    _model.ItemPropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(_model_ItemPropertyChanged);
+                    _model.PropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(_model_PropertyChanged);
+                }
+                _model = value;
+
+                refreshFromModel();
+
+                _model.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(_model_CollectionChanged);
+                _model.ItemPropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(_model_ItemPropertyChanged);
+                _model.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(_model_PropertyChanged);
+            }
+        }
+
+
+        private void refreshFromModel() {
+            ItemStack.Children.Clear();
+            foreach (IModelItem item in _model.GenericList) {
+                addItem(item, -1);
+            }
+        }
+
+        private void addItems(IList items, int index) {
+            foreach (IModelItem item in items) {
+                addItem(item, index);
+            }
+        }
+        private void addItem(IModelItem item, int index) {
+            ModelListViewItem view_item = TemplateItem.CreateItem();
+            view_item.DataSource = item;
+            view_item.Parent = this;
+            ItemStack.Children.Add(view_item);
+        }
+
+        void _model_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        void _model_ItemPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            //throw new NotImplementedException();
+        }
+
+        void _model_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            switch(e.Action) {
+                case NotifyCollectionChangedAction.Reset:
+                    refreshFromModel();
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                    addItems(e.NewItems, e.NewStartingIndex);
+                    break;
+                default:
+                throw new NotImplementedException();
+            }
+        }
+
+        private void UserControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+
+            throw new NotImplementedException();
+
+        }
+
+        private ModelListViewItem last_clicked = null;
+
+        public void ItemWasClicked(ModelListViewItem clicked) {
+            lock (ItemStack.Children) {
+                bool set_to = false;
+                switch (Keyboard.Modifiers) {
+                    case ModifierKeys.Control:
+                        clicked.IsSelected = !clicked.IsSelected;
+                        if(clicked.IsSelected)
+                            last_clicked = clicked;
+                        break;
+                    case ModifierKeys.Shift:
+                        foreach (ModelListViewItem item in ItemStack.Children) {
+                            if (item == last_clicked) {
+                                item.IsSelected = true;
+                                set_to = !set_to;
+                            } else   if (item == clicked) {
+                                item.IsSelected = true;
+
+
+                                set_to = !set_to;
+
+                            } else {
+                                item.IsSelected = set_to;
+                            }
+                        }
+                        break;
+                    default:
+                        foreach (ModelListViewItem item in ItemStack.Children) {
+                            item.IsSelected = item == clicked;
+                        }
+                        last_clicked = clicked;
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ModelListViewItem.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MVC.WPF {
+    public class ModelListViewItem : UserControl {
+        public ModelListView Parent;
+
+        private IModelItem _data;
+        public IModelItem DataSource {
+            set {
+                if (_data != null)
+                    _data.PropertyChanged -= new System.ComponentModel.PropertyChangedEventHandler(DataSource_PropertyChanged);
+                _data = value;
+                LoadSourceData();
+                _data.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(DataSource_PropertyChanged);
+            }
+            get {
+                return _data;
+            }
+        }
+
+        public new object Content {
+            get {
+                return contentLabel.Content;
+            }
+            set {
+                contentLabel.Content = value;
+            }
+        }
+
+
+        protected Rectangle BackgroundRectangle = new Rectangle();
+        protected Label contentLabel = new Label();
+
+        public ModelListViewItem() {
+            this.Margin = new Thickness(1);
+            this.Background = BackgroundColor;
+            BackgroundRectangle.Fill = BackgroundColor;
+            BackgroundRectangle.RadiusX = 5;
+            BackgroundRectangle.RadiusY = BackgroundRectangle.RadiusX;
+
+            contentLabel.Background = new SolidColorBrush(Colors.Transparent);
+
+            Grid grid = new Grid();
+            grid.Children.Add(BackgroundRectangle);
+            grid.Children.Add(contentLabel);
+
+            base.Content = grid;
+            this.MouseDown += new MouseButtonEventHandler(UserControl_MouseLeftButtonDown);
+        }
+
+        protected virtual void DataSource_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            switch (e.PropertyName) {
+                case "IsSelected":
+                    if (DataSource.IsSelected) {
+                        BackgroundRectangle.Fill = SelectedBackgroundColor;
+                        changeForegroundColor(SelectedTextColor);
+                    } else {
+                        BackgroundRectangle.Fill = BackgroundColor;
+                        changeForegroundColor(TextColor);
+                    }
+                    break;
+            }
+        }
+
+        protected virtual void changeForegroundColor(Brush brush) {
+            contentLabel.Foreground = brush;
+
+        }
+
+        protected virtual Brush SelectedTextColor {
+            get {
+                return new SolidColorBrush(Colors.White);
+            }
+        }
+        protected virtual Brush TextColor {
+            get {
+                return new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        protected virtual Brush SelectedBackgroundColor {
+            get {
+                return new SolidColorBrush(Colors.DarkBlue);
+            }
+        }
+
+        protected virtual Brush BackgroundColor {
+            get {
+                return new SolidColorBrush(Colors.Transparent);
+            }
+        }
+
+        public bool IsSelected {
+            get {
+                return DataSource.IsSelected;
+            }
+            set {
+                DataSource.IsSelected = value;
+            }
+        }
+
+
+        public virtual ModelListViewItem CreateItem() {
+            return new ModelListViewItem();
+        }
+
+
+        protected void TopLevelGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+        }
+
+        protected void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            if (Parent != null) {
+                Parent.ItemWasClicked(this);
+            } else {
+                IsSelected = !IsSelected;
+            }
+        }
+
+
+
+        public virtual void LoadSourceData() {
+            Content = DataSource;
+        }
+
+        protected System.Windows.Media.Color convertColor(System.Drawing.Color color) {
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
